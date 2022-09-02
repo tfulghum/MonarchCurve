32c2420255ec8dd490eb406aed582cac10cd36ec
tdfulghum <tylerdfulghum@gmail.com>
2022-09-02 15:37:58 -0500

    graphs for kacie

--- main.py
+++ main.py
@@ -1,15 +1,33 @@
 from collate_log import read_data, combine_folder, combine_logs, pd_to_excel
-
+from matplotlib import pyplot as plt
+import pandas as pd
+from datetime import datetime
 
 # by Tyler Fulghum, monarch curve testing section.
 # Reads text file into pandas dataframe
 
 def main():
-    df = combine_folder("7_28_1300_1500")
-    df = combine_logs("7_28_1300_1500")
-    print(df)
-    #fname = "test1"
-    #pd_to_excel(df, fname)
+    df = combine_logs("08_31_2022")
+    strtime = df["Time [UTC]"]
+    strtime = strtime.tolist()
+    i = 0
+
+    for x in strtime:
+        strtime[i] = strtime[i].split(" ")
+        strtime[i] = strtime[i][1]
+        strtime[i] = strtime[i].replace(":", "")
+        strtime[i] = int(strtime[i])
+        i += 1
+
+    x = df[" White_u"]
+    print(x)
+    print(type(x))
+
+    plt.title("Matplotlib demo")
+    plt.xlabel("Time")
+    plt.ylabel("White_u")
+    plt.plot(strtime, x)
+    plt.show()
 
 
 if __name__ == "__main__":
