df13493991ebf412ce43bd2634e8ef2ef511e7a2
Tyler Fulghum <tylerdfulghum@gmail.com>
2022-09-12 14:27:38 -0500

    Rough cleaning function put in place. Cleaning normally uses the 1.5 * quartile approach, but that did not get values that are confirmed correct so using a 3x modifier instead.

--- main.py
+++ main.py
@@ -1,36 +1,19 @@
 from collate_log import read_data, combine_folder, combine_logs, pd_to_excel
+from monarch_func import ignore_outliers
 from matplotlib import pyplot as plt
 import pandas as pd
 from datetime import datetime
+
 
 # by Tyler Fulghum, monarch curve testing section.
 # Reads text file into pandas dataframe
 
 def main():
     df = combine_logs("08_31_2022")
-    strtime = df["Time [UTC]"]
-    strtime = strtime.tolist()
-    i = 0
-
-    for x in strtime:
-        strtime[i] = strtime[i].split(" ")
-        strtime[i] = strtime[i][1]
-        strtime[i] = strtime[i].replace(":", "")
-        strtime[i] = int(strtime[i])
-        i += 1
-
-    x = df[" White_u"]
-    print(x)
-    print(type(x))
-
-    pd_to_excel(df, "8_22")
-
-    plt.title("Matplotlib demo")
-    plt.xlabel("Time")
-    plt.ylabel("White_u")
-    plt.plot(strtime, x)
+    df = ignore_outliers(df," UVB_u")
+    print(df)
+    df.boxplot(" UVB_u")
     plt.show()
-
 
 if __name__ == "__main__":
     main()
---
+++ monarch_func.py
@@ -0,0 +1,17 @@
+import numpy as np
+
+# Receives a collated pandas dataframe created by the combine_logs function in collate_log.py
+
+def ignore_outliers(df, field):
+    # Calculating 1st and 3rd quartiles
+    for x in [field]:
+        q75, q25 = np.percentile(df.loc[:, x], [75, 25])
+        intr_qr = q75 - q25
+
+        max = q75 + (3 * intr_qr)
+        min = q25 - (3 * intr_qr)
+
+        df.loc[df[x] < min, x] = np.nan
+        df.loc[df[x] > max, x] = np.nan
+
+    return df
